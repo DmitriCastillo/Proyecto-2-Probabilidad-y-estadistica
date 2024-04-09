64b634143e5a3543e3d65d359e0cb18e0ca9e440
DmitriCastillo <160551322+DmitriCastillo@users.noreply.github.com>
2024-04-08 21:04:50 -0600

    Update Proyecto2_SPP.py

--- Proyecto2_SPP.py
+++ Proyecto2_SPP.py
@@ -1,8 +1,11 @@
 '''
+
+
     En este programa se van a calcular la probabilidades de los dadas y las funciones de masa de probabilidad
 
 
 '''
+
 import math
 import numpy as np
 from matplotlib import pyplot as plt
@@ -25,43 +28,46 @@ probabilidad_DadoTrucado2=[0.06,0.06,0.06,0.7,0.06,0.06]
 
 #Dado 1
 plt.figure()
-plt.plot(Cara_Dado,probabilidad_Dado1, 'bo')
+plt.stem(Cara_Dado,probabilidad_Dado1, 'bo', basefmt=' ')
 plt.title('Dado ideal 1')
-plt.xlabel('Caras de dado')
+plt.xlabel('Caras del dado')
 plt.ylabel('Probabilidad')
-plt.yticks([1/6,1/6,1/6,1/6,1/6,1/6],['1/6','1/6','1/6','1/6','1/6','1/6'])
+plt.yticks([0,1/6,1],['0','1/6','1'])
 #Dado 2
 plt.figure()
-plt.plot(Cara_Dado,probabilidad_Dado2, 'bo')
+plt.stem(Cara_Dado,probabilidad_Dado2, 'bo', basefmt=' ')
 plt.title('Dado ideal 2')
-plt.xlabel('Caras de dado')
+plt.xlabel('Caras del dado')
 plt.ylabel('Probabilidad')
-plt.yticks([1/6,1/6,1/6,1/6,1/6,1/6],['1/6','1/6','1/6','1/6','1/6','1/6'])
+plt.yticks([0,1/6,1],['0','1/6','1'])
 
 #Dado trucado 1
 plt.figure()
-plt.plot(Cara_Dado,probabilidad_DadoTrucado1, 'bo')
+plt.stem(Cara_Dado,probabilidad_DadoTrucado1, 'bo', basefmt=' ')
 plt.title('Dado trucado 1')
-plt.xlabel('Caras de dado')
+plt.xlabel('Caras del dado')
 plt.ylabel('Probabilidad')
-plt.yticks([0.06,0.06,0.7,0.06,0.06,0.06],['0.06','0.06','0.7','0.06','0.06','0.06'])
+plt.yticks([0,0.06,0.7,1],['0','0.06','0.7','1'])
 #Dado trucado 2
 plt.figure()
-plt.plot(Cara_Dado,probabilidad_DadoTrucado2, 'bo')
+plt.stem(Cara_Dado,probabilidad_DadoTrucado2, 'bo',basefmt=' ')
 plt.title('Dado trucado 2')
-plt.xlabel('Caras de dado')
+plt.xlabel('Caras del dado')
 plt.ylabel('Probabilidad')
-plt.yticks([0.06,0.06,0.06,0.7,0.06,0.06],['0.06','0.06','0.06','0.7','0.06','0.06'])
+plt.yticks([0,0.06,0.7,1],['0','0.06','0.7','0'])
 
 '''
+
     Ahora graficamos las resultados posibles tirando los dos dados y dadas las probabilidades
     en este caso hay que anailisar todos los posibles resultados que se pueden obtener con dos
     dados ideales y dos dados trucados
+    
 '''
 
 Resultados_Posibles=[2,3,4,5,6,7,8,9,10,11,12]
 
 '''
+
     conociendo los resultados posibles tenemos que calcular la probablidad de cada uno dadas las probabilidades
     de los dos dados ideales y los dos dados trucados
     Entonces
@@ -79,30 +85,32 @@ Resultados_Posibles=[2,3,4,5,6,7,8,9,10,11,12]
     12 tenemos 6:6 una forma
 
     Lo que nos da un total de 36 diferentes posibilidades
+
+    "Saque cada probabilidad a mano"
+    
 '''
 
 Probabilidades_Ideales=[1/36,1/18,1/12,1/9,5/36,1/6,5/36,1/9,1/12,1/18,1/36]
+
 Probabilidades_Trucadas=[9/2500,9/1250,219/2500,21/125,1453/2500,1243/1250,1453/2500,21/125,219/2500,9/1250,9/2500]
 
 #Grafica de probabilidad de los dados ideales
 plt.figure()
-plt.plot(Resultados_Posibles,Probabilidades_Ideales, 'bo')
+plt.stem(Resultados_Posibles,Probabilidades_Ideales, 'bo', basefmt=' ')
 plt.title('Dados Ideales')
 plt.xlabel('Suma de los dados')
 plt.ylabel('Probabilidad')
-plt.yticks([1/36,1/18,1/12,1/9,5/36,1/6,5/36,1/9,1/12,1/18,1/36],['1/36','1/18','1/12','1/9','5/36','1/6','5/36','1/9','1/12','1/18','1/36'])
+plt.yticks([0,1/36,1/18,1/12,1/9,5/36,1/6,5/36,1/9,1/12,1/18,1/36,1],['0','1/36','1/18','1/12','1/9','5/36','1/6','5/36','1/9','1/12','1/18','1/36','1'])
 plt.xticks([2,3,4,5,6,7,8,9,10,11,12])
 
 #Grafica de probabilidad de los dados trucados
 plt.figure()
-plt.plot(Resultados_Posibles,Probabilidades_Trucadas, 'bo')
+plt.stem(Resultados_Posibles,Probabilidades_Trucadas, 'bo', basefmt=' ')
 plt.title('Dados Trucados')
 plt.xlabel('Suma de los dados')
 plt.ylabel('Probabilidad')
-plt.yticks([9/2500,9/1250,219/2500,21/125,1453/2500,1243/1250,1453/2500,21/125,219/2500,9/1250,9/2500],['9/2500','9/1250','219/2500','21/125','1453/2500','1243/1250','1453/2500','21/125','219/2500','9/1250','9/2500'])
+plt.yticks([0,9/2500,9/1250,219/2500,21/125,1453/2500,1243/1250,1453/2500,21/125,219/2500,9/1250,9/2500,1],['0','9/2500','9/1250','219/2500','21/125','1453/2500','1243/1250','1453/2500','21/125','219/2500','9/1250','9/2500','1'])
 plt.xticks([2,3,4,5,6,7,8,9,10,11,12])
 
-
-
+#Mostramos las graficas
 plt.show()
-
